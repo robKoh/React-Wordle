0b6622829695a30ca3443348ce66c4e4871c3a3f
robKoh <robertkohn00@gmail.com>
2022-05-03 19:35:24 +0200

    Added db.json with words, updated App.js, added component Wordle.js and added hook useWordle.js - State: fourth lessson

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react';
 import './App.css'
+import Wordle from './components/Wordle'
 
 function App() {
+  const [solution, setSolution] = useState(null)
+
+
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+      .then(res => res.json())
+      .then(json => {
+        const randSol = json[Math.floor(Math.random() * json.length)]
+        setSolution(randSol.word)
+      })
+  }, [setSolution])
+
   return (
     <div className="App">
       <h1>Wordle (Lingo)</h1>
+      {solution && <Wordle solution={solution}/>}
     </div>
   );
 }
