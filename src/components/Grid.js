b4cfeb6359ae263f9c728c98b6d1a29bf1a49d09
robKoh <robertkohn00@gmail.com>
2022-05-06 14:51:31 +0200

    Now you can type into the grid your current guess which will be displayed.

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import Row from './Row'
+import React from "react";
+import Row from "./Row";
 
 export default function Grid({ currentGuess, guesses, turn }) {
   return (
     <div>
-        {guesses.map( (g, i) => {
-            return <Row key={i} guess={g}/>
-        })}
+      {guesses.map((g, i) => {
+        if (turn === i) {
+          return <Row key={i} currentGuess={currentGuess} />;
+        }
+        return <Row key={i} guess={g} />;
+      })}
     </div>
-  )
+  );
 }
